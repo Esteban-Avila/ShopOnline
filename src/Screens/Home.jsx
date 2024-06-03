e35fce815d3feee406c36566a2eecd118369053f
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-06-03 17:07:38 -0500

    Nuevas funciones

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Headed from '../Components/Headed';
 import NavBar from '../Components/NavBar/NavBar';
 import Social from '../Components/foot/Social';
 import prev from '../assets/previus.png';
 import next from '../assets/next.png';
+import CADS from '../../public/photos/CA-DS-Product.png';
 import '../Style/Home.css';
 import productsData from '../products.json';
+import infoData from '../info.json';
+import Modal from '../Components/modal';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [isButton, setIsButton] = useState(false);
+  const [modalData, setModalData] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    // Simular la carga de datos
     setProducts(productsData);
   }, []);
+
+  const handleMouseEnter = () => {
+    setIsButton(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsButton(false);
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/Products');
+  };
 
   const itemsPerPage = 4;
   const totalPages = Math.ceil(products.length / itemsPerPage);
@@ -30,70 +50,111 @@ const Home = () => {
   const startIndex = currentPage * itemsPerPage;
   const displayedProducts = products.slice(startIndex, startIndex + itemsPerPage);
 
+  const openModal = (linkId) => {
+    setModalData(infoData[linkId]);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalData(null);
+  };
+
   return (
     <div className='container-home'>
-      
       <div>
         <Headed />
         <NavBar />
       </div>
 
       <div className='container-photo'>
-          <h1>COLLARES UNICOS</h1>
-          <button className='more'>MÁS</button>
+        <h1>COLLARES UNICOS</h1>
+        <button className='more' onClick={handleClick}>MÁS</button>
       </div>
-      
-      <div className='content'>
 
+      <div className='content'>
         <h2>MÁS VENDIDOS</h2>
         {displayedProducts.length === 0 ? (
           <p>Cargando productos...</p>
         ) : (
           <div className='products-carousel'>
-
-            <button className="btnprevius" onClick={handlePrev}><img src={prev} alt="btnprev"/></button>
-
+            <button className="btnprevius" onClick={handlePrev}>
+              <img src={prev} alt="btnprev" />
+            </button>
             {displayedProducts.map((product) => (
               <div key={product.id} className='product-card'>
-                <img src={product.image} alt={product.name} className='product-image' /><br></br>
+                <img src={product.image} alt={product.name} className='product-image' /><br />
                 <p>{product.name}</p>
-                <p>{product.price}</p>
+                <p className="bold-text">{product.price}</p>
                 <button className='add-button'>Agregar</button>
               </div>
             ))}
-
-            <button className="buttomnext" onClick={handleNext}><img src={next} alt="btnnext"/></button>
-
+            <button className="buttomnext" onClick={handleNext}>
+              <img src={next} alt="btnnext" />
+            </button>
           </div>
         )}
-
-        <button className="moreP">Más productos</button>
-
+        <button className="moreP" onClick={handleClick}>Más productos</button>
       </div>
 
       <div className='container-comp'>
-        <div class="leftHome">
-          <button className=""></button>
+        <div className="leftHome">
+          <img src={CADS}></img>
+          {isButton ? (
+            <button
+              className="btna"
+              onMouseLeave={handleMouseLeave}
+              onClick={handleClick}
+            >
+              Más productos
+            </button>
+          ) : (
+            <label
+              className="label-style"
+              onMouseEnter={handleMouseEnter}
+            >
+              Varia segun tamaño{'\n\n'}$25.000
+            </label>
+          )}
         </div>
-        <div class="rightHome"><h1>CANDONGAS</h1></div>
-        
+        <div className="rightHome">
+          <h1 className="bold-text">CANDONGAS</h1>
+        </div>
       </div>
 
       <div className='container-comp2'>
-          <div className='sectionsa'>
-
+        <div className='sectionsa'>
+          <div className='link1'>
+            <p>Información</p>
+            <h1>BISUTERIA</h1>
+            <a onClick={() => openModal('link1')}>ver más</a>
           </div>
-
-          <div className='sectionsig'>
-            <h1>SIGUE A MARISELLA EN INSTAGRAM</h1>
-            <a href="https://www.instagram.com/marisella.jewelry?igsh=MWhpYng2aW1yZXhqcA==">@marisella.jewelry</a>
+          <div className='link2'>
+            <p>Productos</p>
+            <h1>ACCESORIOS</h1>
+            <a onClick={() => openModal('link2')}>ver más</a>
           </div>
+          <div className='link3'>
+            <p>Negocio</p>
+            <h1>INFORMACIÓN</h1>
+            <a onClick={() => openModal('link3')}>ver más</a>
+          </div>
+        </div>
+        <div className='sectionsig'>
+          <h1>SIGUE A MARISELLA EN INSTAGRAM</h1>
+          <a href="https://www.instagram.com/marisella.jewelry?igsh=MWhpYng2aW1yZXhqcA==">@marisella.jewelry</a>
+        </div>
       </div>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h1 className='title_M'>{modalData?.title}</h1>
+        <p className='Content_M'>{modalData?.content}</p>
+      </Modal>
 
       <div>
-      <Social />
+        <Social />
       </div>
-    
+
     </div>
   );
 };
