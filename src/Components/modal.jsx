36caee153efa2dec6a21d4e8d86dbecb4b2e6680
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-06-03 17:06:53 -0500

    Ventana emergente

---
+++ src/Components/modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import close from '../assets/close.png';
+import '../Style/Home.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close" onClick={onClose}>
+          <img src={close} alt="Close" />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
