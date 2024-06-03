724384e0c2aa4034e2fa30a28f55fb0806ba270c
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-06-03 12:34:04 -0500

    Nuevos contenedores

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Headed from '../Components/Headed';
 import NavBar from '../Components/NavBar/NavBar';
-import Social from '../Components/Sections/Social';
-import '../StyleScreens/Home.css';
+import Social from '../Components/foot/Social';
+import prev from '../assets/previus.png';
+import next from '../assets/next.png';
+import '../Style/Home.css';
 import productsData from '../products.json';
 
 const Home = () => {
@@ -30,69 +32,68 @@ const Home = () => {
 
   return (
     <div className='container-home'>
+      
       <div>
         <Headed />
         <NavBar />
       </div>
+
+      <div className='container-photo'>
+          <h1>COLLARES UNICOS</h1>
+          <button className='more'>MÁS</button>
+      </div>
+      
       <div className='content'>
-        <h1>Bienvenido</h1>
-        <h2>Aretes</h2>
-        {displayedProducts.length === 0 ? (
-          <p>Cargando productos...</p>
-        ) : (
-          <div className='products-carousel'>
-            {displayedProducts.map((product) => (
-              <div key={product.id} className='product-card'>
-                <img src={product.image} alt={product.name} className='product-image' /><br></br>
-                <button className='add-button'>Agregar</button>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className='carousel-controls'>
-          <button onClick={handlePrev}>Anterior</button>
-          <button onClick={handleNext}>Siguiente</button>
-        </div>
 
-        <h2>Collares</h2>
+        <h2>MÁS VENDIDOS</h2>
         {displayedProducts.length === 0 ? (
           <p>Cargando productos...</p>
         ) : (
           <div className='products-carousel'>
-            {displayedProducts.map((product) => (
-              <div key={product.id} className='product-card'>
-                <img src={product.image} alt={product.name} className='product-image' /><br></br>
-                <button className='add-button'>Agregar</button>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className='carousel-controls'>
-          <button onClick={handlePrev}>Anterior</button>
-          <button onClick={handleNext}>Siguiente</button>
-        </div>
 
-        <h2>Manillas</h2>
-        {displayedProducts.length === 0 ? (
-          <p>Cargando productos...</p>
-        ) : (
-          <div className='products-carousel'>
+            <button className="btnprevius" onClick={handlePrev}><img src={prev} alt="btnprev"/></button>
+
             {displayedProducts.map((product) => (
               <div key={product.id} className='product-card'>
                 <img src={product.image} alt={product.name} className='product-image' /><br></br>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
                 <button className='add-button'>Agregar</button>
               </div>
             ))}
+
+            <button className="buttomnext" onClick={handleNext}><img src={next} alt="btnnext"/></button>
+
           </div>
         )}
-        <div className='carousel-controls'>
-          <button onClick={handlePrev}>Anterior</button>
-          <button onClick={handleNext}>Siguiente</button>
-        </div>
+
+        <button className="moreP">Más productos</button>
+
       </div>
+
+      <div className='container-comp'>
+        <div class="leftHome">
+          <button className=""></button>
+        </div>
+        <div class="rightHome"><h1>CANDONGAS</h1></div>
+        
+      </div>
+
+      <div className='container-comp2'>
+          <div className='sectionsa'>
+
+          </div>
+
+          <div className='sectionsig'>
+            <h1>SIGUE A MARISELLA EN INSTAGRAM</h1>
+            <a href="https://www.instagram.com/marisella.jewelry?igsh=MWhpYng2aW1yZXhqcA==">@marisella.jewelry</a>
+          </div>
+      </div>
+
       <div>
-        <Social />
+      <Social />
       </div>
+    
     </div>
   );
 };
