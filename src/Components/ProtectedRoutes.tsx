d4391752636b8fac72dc8341e81eda2c9ac8973e
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-06-03 09:52:47 -0500

    .

--- src/Components/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet ,  Navigate } from "react-router-dom";
-import { useAuth } from "../Auth/AuthProvider";
-import React from "react";
-export default function protectedroutes(){
-    const auth = useAuth()
-    
-    return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />
-
-}
