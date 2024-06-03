68ed5d2d8ca621bac6507fa553f0f16a65316918
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-06-03 09:53:24 -0500

    importes

--- src/Components/Headed.jsx
+++ src/Components/Headed.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import logo from '../assets/logonom-original.png';
+import logoW from '../assets/whatsapp.png';
+import logoT from '../assets/tiktok.png';
+import logoIg from '../assets/ig.png';
+import logoU from '../assets/user.png';
 import './Headed.css';
+import { Link } from "react-router-dom";
 function Headed(){
     return(
-        <div className="logo-container">
-            <img src={logo} alt="logo" className="logo"/>
+        <div className="top">
+            <div className="logo-container">
+                <img src={logo} alt="logo" className="logo"/>
+            </div>            
+            
+            <div className="btnSocial">   
+                    <a href="https://www.instagram.com/marisella.jewelry?igsh=MWhpYng2aW1yZXhqcA=="><img src={logoIg} className="logos"></img></a>
+                    <a href="https://www.tiktok.com/@marisella.jewelry"><img src={logoT} className="logos"></img></a>
+                    <a href="https://wa.me/573001359534"><img src={logoW} className="logos"></img></a>
+            </div>
+
+            <div className="btn_varius">
+                <ul id="links">
+                    <li><Link id="itemH" to={'/Login'}>Entrar</Link></li>
+                    <li><Link id="itemH" to={'/Login'}>Carrito</Link></li>
+                </ul>
+            </div>
         </div>
     )
 };
